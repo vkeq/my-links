6a718cec6a74e8a5ec4470f58b04851429b7dc2e
@f-sensei <66038700+vkeq@users.noreply.github.com>
2022-04-12 20:25:31 +0200

    Add files via upload

---
+++ assets/config.js
@@ -0,0 +1,22 @@
+let config = {
+  "Title": ".f's links",
+  "Name": ".f#2313",
+  "Description": "student & player",
+  "links": [
+    {
+      "Title": "Website",
+      "URL": "https://f-sensei.xyz",
+      "icon_classes": "fas fa-link",
+    },
+    {
+      "Title": "Github",
+      "URL": "https://github.com/vkeq",
+      "icon_classes": "fab fa-github",
+    },
+    {
+      "Title": "Instagram",
+      "URL": "https://instagram.com/sono.long/",
+      "icon_classes": "fab fa-instagram"
+    },
+  ]
+}
